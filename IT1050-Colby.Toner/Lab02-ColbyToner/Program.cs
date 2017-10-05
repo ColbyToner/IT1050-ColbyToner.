ad872ce14f36eebe940beb168f4d1f18de2076c9
ColbyToner <32346680+ColbyToner@users.noreply.github.com>
2017-10-04 22:33:15 -0400

    Add files via upload

---
+++ IT1050-Colby.Toner/Lab02-ColbyToner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_ColbyToner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number :");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number :");
+            int number2 = int.Parse(Console.ReadLine());
+            int sum = number1 + number2;
+            Console.WriteLine("Your first number plus your second number is " + sum);
+            int x = 3;
+            int y = 4;
+            int z = 5;
+            Console.WriteLine((x + y) * (z + 10));
+            Console.WriteLine("Hello \t World ");
+            Console.WriteLine("Hello \nWorld ");
+            Console.WriteLine("\"Hello World!\" ");
+            Console.WriteLine("Hello\\World! ");
+            
+        }
+    }
+}
---
+++ IT1050-Colby.Toner/Lab02-ColbyToner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab02-ColbyToner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Lab02-ColbyToner")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5cb6fa8a-2678-41a4-bb7d-78351b0c1b67")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
